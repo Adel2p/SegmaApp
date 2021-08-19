33d64efc74203a1ebd76295436071fc25997d0f9
hebasamimohamed <hebasami303@gmail.com>
2021-08-19 12:43:30 +0200

    reverse mohamed updates

---
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/Splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.noob.apps.mvvmcountries.ui.Splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noob.apps.mvvmcountries.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+    }
+}
