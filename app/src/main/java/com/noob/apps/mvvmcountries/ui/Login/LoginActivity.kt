84d2d0482eae38f6fd636b35594c97ba7250711d
hebasamimohamed <hebasami303@gmail.com>
2021-08-22 20:57:30 +0200

    the login activity

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/Login/LoginActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/Login/LoginActivity.kt
@@ -10,6 +10,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 
     override fun onBackPressed() {
---
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/Login/Visitor/Visitor.kt
@@ -0,0 +1,13 @@
+package com.noob.apps.mvvmcountries.ui.Login.Visitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noob.apps.mvvmcountries.R
+
+class Visitor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visitor)
+
+    }
+}
