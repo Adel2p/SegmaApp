3f60827a7b8be841cc3a097efa1d8759156998ed
hebasamimohamed <hebasami303@gmail.com>
2021-08-24 10:24:13 +0200

    the login activity

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/login/LoginActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/login/LoginActivity.kt
@@ -8,6 +8,7 @@ import com.noob.apps.mvvmcountries.R
 import com.noob.apps.mvvmcountries.databinding.ActivityCountriesListBinding
 import com.noob.apps.mvvmcountries.databinding.ActivityLoginBinding
 import com.noob.apps.mvvmcountries.ui.CountriesListActivity
+import com.noob.apps.mvvmcountries.ui.visitor.Visitor
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var mActivityBinding: ActivityLoginBinding
@@ -15,21 +16,17 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mActivityBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_login)
-//       val myButton1 = findViewById<View>(R.id.loginButton)
-//       val signin_textview =findViewById<View>(R.id.txtCreateNewAccount)
-//       myButton1.setOnClickListener {
-//           startActivity(Intent(this@LoginActivity, Visitor::class.java))
-//       }
-//        signin_textview.setOnClickListener{
-//
-//            startActivity(Intent(this@LoginActivity, Visitor::class.java))
-//       }
+
+        mActivityBinding.txtVisitor.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, Visitor::class.java))
+        }
+
 
     }
 
     override fun onBackPressed() {
 
-        val intent= Intent(this@LoginActivity, CountriesListActivity::class.java)
+        val intent = Intent(this@LoginActivity, CountriesListActivity::class.java)
         startActivity(intent)
 
     }
