42c6e734f4dd16daeea0c52fea39388ebea61446
Mohamed.Adel <mohmmed.adel99@gmail.com>
2021-08-24 14:26:45 +0200

    fix update issue

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/Splash/SplashActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/Splash/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.noob.apps.mvvmcountries.R
 import com.noob.apps.mvvmcountries.ui.TestActivity
+import com.noob.apps.mvvmcountries.ui.login.LoginActivity
 import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
 
@@ -15,8 +16,7 @@ class SplashActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             delay(TimeUnit.SECONDS.toMillis(3))
             withContext(Dispatchers.Main) {
-                val intent = Intent(this@SplashActivity,TestActivity::class.java)
-
+                val intent = Intent(this@SplashActivity,LoginActivity::class.java)
                 startActivity(intent)
                 finish()
             }
