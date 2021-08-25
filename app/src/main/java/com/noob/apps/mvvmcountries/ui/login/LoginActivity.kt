f7d7666c8048ef9ad11210b1f7baae3534b0b710
hebasamimohamed <hebasami303@gmail.com>
2021-08-25 14:24:28 +0200

    add mobile number validator

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/login/LoginActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/login/LoginActivity.kt
@@ -7,14 +7,13 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.noob.apps.mvvmcountries.R
 import com.noob.apps.mvvmcountries.databinding.ActivityLoginBinding
-import com.noob.apps.mvvmcountries.ui.CountriesListActivity
 import com.noob.apps.mvvmcountries.ui.signup.SignUpActivity
 import com.noob.apps.mvvmcountries.ui.visitor.VisitorActivity
 import com.noob.apps.mvvmcountries.utils.MobileNumberValidator
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var mobileNumber:String
-    private lateinit var password:String
+    private lateinit var mobileNumber: String
+    private lateinit var password: String
     private lateinit var mActivityBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,19 +28,16 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
         }
         mActivityBinding.loginButton.setOnClickListener {
-            mobileNumber=mActivityBinding.etMobileNumber.text.toString()
-            password=mActivityBinding.etPassword.text.toString()
-            Toast.makeText(this,mobileNumber,Toast.LENGTH_LONG).show()
-           if( MobileNumberValidator.validCellPhone(mobileNumber)){
-               Toast.makeText(this,mobileNumber,Toast.LENGTH_LONG).show()
+            mobileNumber = mActivityBinding.etMobileNumber.text.toString()
+            password = mActivityBinding.etPassword.text.toString()
+            if (MobileNumberValidator.validCellPhone(mobileNumber)) {
+                Toast.makeText(this, mobileNumber, Toast.LENGTH_LONG).show()
+            } else {
+                mActivityBinding.etMobileNumber.error = "Invalid Mobile Number"
+            }
+
+
         }
-            else{
-                mActivityBinding.inputMobile.error="Invalid Mobile Number"
-           }
-
-
-
-
-    }}
+    }
 
 }
