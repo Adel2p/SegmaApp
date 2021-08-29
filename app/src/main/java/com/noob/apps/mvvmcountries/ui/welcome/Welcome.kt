9bee56199186d1b3d741232045e5393b2db5d9d1
hebasamimohamed <hebasami303@gmail.com>
2021-08-29 20:37:45 +0200

    fix login & visitor & signup validation

---
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/welcome/Welcome.kt
@@ -0,0 +1,25 @@
+package com.noob.apps.mvvmcountries.ui.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Welcome : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
--- app/src/main/java/com/noob/apps/mvvmcountries/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/welcome/WelcomeActivity.kt
@@ -2,19 +2,11 @@ package com.noob.apps.mvvmcountries.ui.welcome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentManager
 import com.noob.apps.mvvmcountries.R
-import com.noob.apps.mvvmcountries.databinding.CountriesListItemBinding
 
 class WelcomeActivity : AppCompatActivity() {
-    private lateinit var mActivityBinding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mActivityBinding =
-            DataBindingUtil.setContentView(this, R.layout.fragment_welcome)
-        mActivityBinding.continueButton.setOnClickListener {
-
-        }
+        setContentView(R.layout.activity_welcome)
     }
 }
