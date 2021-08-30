ab734d0249c802e34ea7c0e881b13ed4b72c14e7
hebasamimohamed <hebasami303@gmail.com>
2021-08-30 12:42:27 +0200

    fix login & visitor & signup validation

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/signup/VerifyOtpActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/signup/VerifyOtpActivity.kt
@@ -2,11 +2,20 @@ package com.noob.apps.mvvmcountries.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.noob.apps.mvvmcountries.R
+import com.noob.apps.mvvmcountries.databinding.ActivityVerifyOtpBinding
 
 class VerifyOtpActivity : AppCompatActivity() {
+    private lateinit var mActivityBinding: ActivityVerifyOtpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verify_otp)
+        mActivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_verify_otp)
+        val mobileNumber = intent.getStringExtra("MOBILE_NUMBER")
+        mActivityBinding.txtMobileNumber.text = mobileNumber.toString()
+        mActivityBinding.txtChangeNumber.setOnClickListener {
+            finish()
+        }
     }
 }
