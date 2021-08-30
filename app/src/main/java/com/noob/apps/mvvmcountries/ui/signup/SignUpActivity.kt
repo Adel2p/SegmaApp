a12c24ff1c39bcd44a681e2af6ec986924aa40c7
hebasamimohamed <hebasami303@gmail.com>
2021-08-30 15:53:24 +0200

    fix login & visitor & signup validation

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/signup/SignUpActivity.kt
@@ -29,12 +29,12 @@ class SignUpActivity : AppCompatActivity() {
             eMail = mActivityBinding.etemail.text.toString()
             mobileNumber = mActivityBinding.etMobileNumber.text.toString()
             password = mActivityBinding.etPassword.text.toString()
-            if (checkValidation()) {
+           // if (checkValidation()) {
                 val intent = Intent(this@SignUpActivity, VerifyOtpActivity::class.java)
                 intent.putExtra("MOBILE_NUMBER", mobileNumber)
                 startActivity(intent)
 
-            }
+          //  }
         }
     }
 
--- app/src/main/java/com/noob/apps/mvvmcountries/ui/signup/VerifyOtpActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/signup/VerifyOtpActivity.kt
@@ -2,9 +2,14 @@ package com.noob.apps.mvvmcountries.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.noob.apps.mvvmcountries.R
 import com.noob.apps.mvvmcountries.databinding.ActivityVerifyOtpBinding
+import com.mukesh.OnOtpCompletionListener
+
+
+
 
 class VerifyOtpActivity : AppCompatActivity() {
     private lateinit var mActivityBinding: ActivityVerifyOtpBinding
@@ -16,6 +21,14 @@ class VerifyOtpActivity : AppCompatActivity() {
         mActivityBinding.txtMobileNumber.text = mobileNumber.toString()
         mActivityBinding.txtChangeNumber.setOnClickListener {
             finish()
+        }
+mActivityBinding.otpView.onc
+        mActivityBinding.otpView.setListener(OnOtpCompletionListener { otp -> // do Stuff
+            mActivityBinding.confirmButton.setImageResource(R.drawable.curved_button_blue);
+        })
+        mActivityBinding.confirmButton.setOnClickListener{
+        val otp =mActivityBinding.otpView.text.toString()
+            Toast.makeText(this,otp,Toast.LENGTH_LONG).show()
         }
     }
 }
