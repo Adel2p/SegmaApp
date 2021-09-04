2a77f52656cd2f61a7eb59df7a253c6b7d9c6563
hebasamimohamed <hebasami303@gmail.com>
2021-09-04 12:17:34 +0200

    fix forgetPassword and ForgetPasswordReset Activities

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/home/HomeActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/home/HomeActivity.kt
@@ -3,8 +3,12 @@ package com.noob.apps.mvvmcountries.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.noob.apps.mvvmcountries.R
 import com.noob.apps.mvvmcountries.databinding.ActivityHomeBinding
+import com.noob.apps.mvvmcountries.ui.course.CoursesFragment
+import com.noob.apps.mvvmcountries.ui.library.LibraryFragment
+import com.noob.apps.mvvmcountries.ui.more.MoreFragment
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var mActivityBinding:ActivityHomeBinding
@@ -12,5 +16,25 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mActivityBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_home)
+        val homeFragment=HomeFragment()
+        val coursesFragment=CoursesFragment()
+        val libraryFragment=LibraryFragment()
+        val moreFragment=MoreFragment()
+        setCurrentFragment(homeFragment)
+        mActivityBinding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(homeFragment)
+                R.id.courses->setCurrentFragment(coursesFragment)
+                R.id.library->setCurrentFragment(libraryFragment)
+                R.id.more->setCurrentFragment(moreFragment)
+
+            }
+            true
+        }
     }
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container,fragment)
+            commit()
+        }
 }
--- app/src/main/java/com/noob/apps/mvvmcountries/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/splash/SplashActivity.kt
@@ -3,12 +3,10 @@ package com.noob.apps.mvvmcountries.ui.splash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.lifecycle.asLiveData
 import com.noob.apps.mvvmcountries.R
-import com.noob.apps.mvvmcountries.ui.forgetpassword.ForgetPasswordActivity
+import com.noob.apps.mvvmcountries.ui.home.HomeActivity
 import com.noob.apps.mvvmcountries.ui.login.LoginActivity
-import com.noob.apps.mvvmcountries.ui.main.MainActivity
 import com.noob.apps.mvvmcountries.ui.welcome.UniversityActivity
 import com.noob.apps.mvvmcountries.utils.UserPreferences
 import kotlinx.coroutines.*
@@ -34,7 +32,7 @@ class SplashActivity : AppCompatActivity() {
             withContext(Dispatchers.Main) {
                 if(isloggedin) {
                     if (isSaved) {
-                        val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                        val intent = Intent(this@SplashActivity, HomeActivity::class.java)
 
                         startActivity(intent)
                         finish()
