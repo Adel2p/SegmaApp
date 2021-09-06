095a0ec9f83264e83d9daa81bc7f16f088ff64a0
hebasamimohamed <hebasami303@gmail.com>
2021-09-06 20:39:21 +0200

    add lecture home screen

---
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/more/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.noob.apps.mvvmcountries.ui.more
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noob.apps.mvvmcountries.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
