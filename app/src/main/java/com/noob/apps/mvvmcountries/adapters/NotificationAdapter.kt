c970e4403c0060ce1f6a34649fb7c379aa51fcfe
hebasamimohamed <hebasami303@gmail.com>
2021-09-08 18:11:07 +0200

    add lectu re home screen

--- app/src/main/java/com/noob/apps/mvvmcountries/adapters/NotificationAdapter.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/adapters/NotificationAdapter.kt
@@ -1,4 +1,49 @@
 package com.noob.apps.mvvmcountries.adapters
 
-class NotificationAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.noob.apps.mvvmcountries.R
+import com.noob.apps.mvvmcountries.databinding.NotificationCellBinding
+import com.noob.apps.mvvmcountries.models.Notification
+import kotlinx.android.extensions.LayoutContainer
+
+class NotificationAdapter: RecyclerView.Adapter<NotificationAdapter.ViewHolder>() {
+
+    private var mList: List<Notification>? = listOf()
+
+    fun setData(list: List<Notification>) {
+        mList = list
+        notifyItemRangeChanged(0, mList!!.size)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding: NotificationCellBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.notification_cell,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return mList!!.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemBinding.notification = mList!![position]
+    }
+
+    class ViewHolder(var itemBinding: NotificationCellBinding) :
+        RecyclerView.ViewHolder(itemBinding.root), LayoutContainer {
+        override val containerView: View?
+            get() = itemBinding.root
+    }
+
 }
--- app/src/main/java/com/noob/apps/mvvmcountries/ui/more/NotificationActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/more/NotificationActivity.kt
@@ -2,15 +2,51 @@ package com.noob.apps.mvvmcountries.ui.more
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import com.noob.apps.mvvmcountries.R
+import com.noob.apps.mvvmcountries.adapters.FavouriteLectureAdapter
+import com.noob.apps.mvvmcountries.adapters.NotificationAdapter
 import com.noob.apps.mvvmcountries.databinding.ActivityFavouriteLectureBinding
 import com.noob.apps.mvvmcountries.databinding.ActivityNotificationBinding
+import com.noob.apps.mvvmcountries.models.Lecture
+import com.noob.apps.mvvmcountries.models.Notification
 
 class NotificationActivity : AppCompatActivity() {
+    private lateinit var mAdapter: NotificationAdapter
+    private val listOfNotifications: MutableList<Notification> = mutableListOf()
     private lateinit var mActivityBinding: ActivityNotificationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+        mActivityBinding =
+            DataBindingUtil.setContentView(this, R.layout.notification_cell)
+        val notification1 = Notification("computer science", "Content")
+        val notification2 = Notification("computer science", "content")
+        val notification3 = Notification("computer science", "content")
+        val notification4 = Notification("computer science", "content")
+        val notification5 = Notification("computer science", "content")
+        val notification6 = Notification("computer science", "content")
+        val notification7 = Notification("computer science", "content")
+
+        listOfNotifications.add(notification1)
+        listOfNotifications.add(notification2)
+        listOfNotifications.add(notification3)
+        listOfNotifications.add(notification4)
+        listOfNotifications.add(notification5)
+        listOfNotifications.add(notification6)
+        listOfNotifications.add(notification7)
+        initializeRecyclerView()
+        mAdapter.setData(listOfNotifications)
+
+    }
+    private fun initializeRecyclerView() {
+        mAdapter = NotificationAdapter()
+        mActivityBinding.notificationRec.apply {
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(this@NotificationActivity, 1)
+
+            adapter = mAdapter
+        }
     }
 }
