9862524e41a67df9fb32ffb752ff0d759e425bf9
Mohamed.Adel <mohmmed.adel99@gmail.com>
2021-09-26 20:35:03 +0200

    add picaso

--- app/src/main/java/com/noob/apps/mvvmcountries/models/BindingAdapters.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/models/BindingAdapters.kt
@@ -9,13 +9,6 @@ object BindingAdapters {
     @JvmStatic
     fun loadImage(imageView: ImageView, imageUrl: String?) {
         if (!imageUrl.isNullOrEmpty()) {
-//            SvgLoader.pluck()
-//                .with(imageView.context as Activity?)
-//                .setPlaceHolder(
-//                    com.noob.apps.mvvmcountries.R.mipmap.ic_launcher,
-//                    com.noob.apps.mvvmcountries.R.mipmap.ic_launcher
-//                )
-//                .load(imageUrl, imageView)
             Picasso.get().load(imageUrl).into(imageView);
 
         }
