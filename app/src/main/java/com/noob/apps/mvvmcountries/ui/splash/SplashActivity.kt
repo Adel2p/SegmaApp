02ed544692d195351c1398cbff46e5f7921dab82
Mohamed.Adel <mohmmed.adel99@gmail.com>
2021-09-27 19:07:42 +0200

    add video resolutions

--- app/src/main/java/com/noob/apps/mvvmcountries/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/noob/apps/mvvmcountries/ui/splash/SplashActivity.kt
@@ -58,7 +58,7 @@ class SplashActivity : BaseActivity() {
 
     private fun readToken() {
         userPreferences.getRefreshToken.asLiveData().observeOnce(this, {
-            refreshToken = it
+       //     refreshToken = it
             if (refreshToken.isNotEmpty())
                 initTokenObservers()
             else
